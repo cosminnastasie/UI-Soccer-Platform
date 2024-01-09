1a5aca2c13f533d9a32d3ead0965e9a575b78905
NASTASIE COSMIN ALIN <cosmin.nastasie@priceedge.se>
2024-01-09 11:28:03 +0200

    Add sass
    Add app background image

--- soccer-platform/src/App.js
+++ soccer-platform/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import './scss/style.scss';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Best Soccer Platform</h1>
+        <h1>Soccer Platform</h1>
       </header>
     </div>
   );
