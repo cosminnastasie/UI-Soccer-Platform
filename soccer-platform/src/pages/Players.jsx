a24017b8ddc665cf35b9f0c3feaf9326646bcd19
NASTASIE COSMIN ALIN <cosmin.nastasie@priceedge.se>
2024-01-11 00:05:12 +0200

    Add ag grid

--- soccer-platform/src/pages/Players.jsx
+++ soccer-platform/src/pages/Players.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AgGridReact } from 'ag-grid-react'; // React Grid Logic
+import "ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
 
 class Players extends React.Component {
   constructor(props) {
@@ -23,6 +26,13 @@ class Players extends React.Component {
       .catch(error => console.log('error', error));
   }
 
+  colDefs = [
+    {field: 'Name'},
+    {field: 'Position1'},
+    {field: 'YearOfBirth'},
+    {field: 'Number'},
+  ]
+
   render() {
     console.log('state', this.state);
     return (
@@ -30,11 +40,9 @@ class Players extends React.Component {
         <h1>Players</h1>
         {
           this.state?.players &&
-          <>
-            {this.state?.players.map(p => {
-              return <div>{p.Name}</div>
-            })}
-          </>
+           <div class="grid-height">
+                  <AgGridReact rowData={this.state?.players} columnDefs={this.colDefs} />
+          </div>
         }
       </div>
     );
