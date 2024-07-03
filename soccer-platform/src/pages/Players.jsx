6cdd704ca767d59362f7e0138bfa97c8f5d1c443
NASTASIE COSMIN ALIN <cosmin.nastasie@priceedge.se>
2024-07-03 16:19:25 +0300

    Update Players.jsx

--- soccer-platform/src/pages/Players.jsx
+++ soccer-platform/src/pages/Players.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { AgGridReact } from 'ag-grid-react'; // React Grid Logic
 import "ag-grid-community/styles/ag-grid.css"; // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
-import { getData } from './../requests/requests';
+import { getData, postData } from './../requests/requests';
 import { URLS } from './../requests/constants'
 import { Checkbox } from '@blueprintjs/core';
 
@@ -12,7 +12,7 @@ class Players extends React.Component {
 		this.state = {
 
 		}
-		this.playerStatusHandler = this.playerStatusHandler.bind(this);
+		this.playerInfoChange = this.playerInfoChange.bind(this);
 
 	}
 	componentDidMount() {
@@ -26,8 +26,35 @@ class Players extends React.Component {
 		});
 	}
 
-	playerStatusHandler = (params) => {
-		console.log(params)
+	playerInfoChange = (params) => {
+		console.log(params);
+		let selectedPlayer = this.state.players.filter(p => p.IdPlayers === params.data.IdPlayers);
+		let selectedPlayerId = selectedPlayer[0].IdPlayers;
+		let field = params.colDef.field;
+		let newValue = params.data[field];
+		if(field === 'IsEnable'){
+			newValue = selectedPlayer[0].IsEnable === 1? 0: 1;
+		}
+
+		postData('update-player', { playerId: selectedPlayerId, field, newValue }).then((result) => {
+			console.log('Result...', result);
+
+			
+		})
+
+
+		let players = this.state.players.map(p=>{
+			if(p.IdPlayers === params.data.IdPlayers){
+				p.IsEnable  = p.IsEnable === 1? 0: 1
+			}
+			return p;
+		});
+		this.setState({players})
+		// let selectedPlayer = this.state.players.filter(p => p.IdPlayers === params.data.IdPlayers);
+	
+		// selectedPlayer.
+		// console.log({selectedPlayerId, selectedPlayer, isEnabled})
+		console.log(this.state)
 	}
 
 
@@ -62,7 +89,7 @@ class Players extends React.Component {
 					console.log({val, params})
 					return <Checkbox
 						checked={val}
-						onChange={()=>_that.playerStatusHandler(params)}
+						onChange={()=>_that.playerInfoChange(params)}
 						style={{ margin: 0 }}
 					/>
 				}
