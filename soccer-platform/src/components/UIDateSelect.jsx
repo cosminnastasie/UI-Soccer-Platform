b8759d86e6b1a0e910e54fa9934b92e56e0d4a5d
NASTASIE COSMIN ALIN <cosmin.nastasie@priceedge.se>
2025-05-29 15:06:40 +0300

    UI

--- soccer-platform/src/components/UIDateSelect.jsx
+++ soccer-platform/src/components/UIDateSelect.jsx
@@ -16,9 +16,9 @@ class UIDateSelect extends Component {
 	  };
     render() {
         return (
-            <div className='sp-btn-group'>
-                <div className='btn-text'>Date: </div>
-                <input type="date" className="sp-btn" ref={this.dateInputRef} 
+            <div className='mc-btn-group'>
+                <div className='btn-text sm-hide'>Date: </div>
+                <input type="date" className="" ref={this.dateInputRef} 
                     value={this.props.value} id={this.props.id} name={this.props.name} 
                     onChange={(event) => { this.props.onChange(event)}} />
             </div>
